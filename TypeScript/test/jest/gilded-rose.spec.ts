507afbb701ef81d8ea49fa535a84c0ba58208000
Julie Montoux <92477225+JulieMontoux@users.noreply.github.com>
2026-07-05 21:02:44 +0200

    feat : ajout test baisse la qualité de 1 par jour

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,16 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
+  // Normal Item
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
+  });
+
+  it('baisse la qualité de 1 par jour', () => {
+    const gildedRose = new GildedRose([new Item('foo', 5, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(9);
   });
 });
